70bdc6e9ce4ffa054c7533a2dea9532d7604b439
fritzvd <f.vandeventer@gmail.com>
2014-10-16 19:15:39 +0200

    all the things and testdata

--- index.js
+++ index.js
@@ -1,18 +1,36 @@
 var io = require('socket.io-client');
+var d3 = require('d3');
+
 
 var btn = document.createElement('button');
+btn.innerHTML = "Hoi";
 document.body.appendChild(btn);
+
+var width = 960,
+    height = 1160;
+
+// var projection = d3.geo.albers()
+//     .center([0, 55.4])
+//     .rotate([4.4, 0])
+//     .parallels([50, 60])
+//     .scale(1200 * 5)
+//     .translate([width / 2, height / 2]);
+
+// var path = d3.geo.path()
+//     .projection(projection);
+
+var path = d3.geo.path()
+    .projection(d3.geo.mercator()
+    .scale(20000)
+    .translate([0, 3800]));
+
+var svg = d3.select("body").append("svg")
+    .attr("width", width)
+    .attr("height", height);
+
 
 
 var socket = io.connect('localhost:5000');
-socket.emit('message', 'piet')
-
-socket.on('message', function (message) {
-  console.log(message, arguments);
-
-});
-
-
 
 btn.onclick = function () {
   socket.emit('gimme'); 
@@ -21,11 +39,28 @@ btn.onclick = function () {
 var osm;
 socket.on('data', function (data) {
   // osm = JSON.parse(data);
-  osm = data.elements;
-  console.log(data, osm)
+  osm = data;
+  data.features.forEach(function (feature) {
+    if (feature.properties.tags.hasOwnProperty('highway')) {
+      return 
+    }
+    console.log(feature)
+  })
+  // debugger
+  // d3.json(osm, function(error, uk) {
+  //   svg.append("g")
+  //       .attr("class", "black")
+  //       .selectAll("path")
+  //       .data(osm.features)
+  //       .enter(function (d) {
+  //         if (!d.properties.tags.hasOwnProperty('highway'))
+  //         return d;
+  //       })
+  //       .append("path")
+  //       .attr("d", path);
+  // });
 });
 
-var d3 = require('d3');
 
-d3.geo.
+
 
--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var o2g = require('osmtogeojson');
 var fs = require('fs');
 
-var data = JSON.parse(fs.readFileSync('./osm.json', 'utf8'));
+var data = JSON.parse(fs.readFileSync('./osm2.json', 'utf8'));
 var res = o2g(data);
 
 
@@ -15,14 +15,9 @@ var server = require('http').createServer(function (request, response) {
 
 var io = require('socket.io').listen(server);
 io.sockets.on('connection', function (socket) {
-  console.log('youre in'  )
-  socket.on('message', function (message) {
-    console.log('Got message: ', message);
-    socket.emit('message', message);
-  });
 
   socket.on('gimme', function (query) {
-    socket.emit('data', data);
+    socket.emit('data', res);
   })
 });
 
